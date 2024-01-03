712868c9404127b39bbdba2332ef034a62132bb4
hrishi1729 <hrishinikam1729@gmail.com>
2024-01-03 13:35:25 +0530

    AddedHeaderComponent

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../Css/header.css'
+
+
+const Header = ()=>{
+    return (
+        <header class="header">
+            <span class="HomeTag">Home</span>
+            <span class="AboutMeTag">AboutMe</span>
+        </header>        
+    );
+}
+
+
+export default Header;
